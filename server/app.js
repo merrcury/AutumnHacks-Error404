3c208fb97f0d0dc9227f00d454b4336c41d2ae6f
RISHU GUPTA <guptarishu04@gmail.com>
2019-10-12 19:57:09 +0530

    routes and crud added

--- server/app.js
+++ server/app.js
@@ -1,11 +1,18 @@
 const express=require('express');
 const app=express();
 app.use(require('./utils/cors'));
+var mail=require('./db/helpers/info');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
-app.use()
+
+app.use('/',require('./routes/user'));
+app.use('/',require('./routes/admin'));
+
+//app.use(require('./utils/jwtmiddleware'));
+app.use('/',require('./routes/police'));
 
 app.listen(process.env.PORT|| 4004,()=>{
     console.log("Server Started, Browse to port no 4004...");
+    mail.sendmail();
 })
